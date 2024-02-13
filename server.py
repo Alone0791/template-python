0ef7283c05da87dbc7144ea055f3b62b0bc4ec35
آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>
2024-02-13 19:47:14 +0530

    Update server.py
    Signed-off-by: آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>

--- server.py
+++ server.py
@@ -13,6 +13,11 @@ def serve_static(path):
 def home():
    return render_template('index.html')
 
+@app.route('/pro')
+def hellopro():
+    return os.system("python html")
+
+
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
