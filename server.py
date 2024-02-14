e839745f255f650003546cb297647da2c996af83
آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>
2024-02-14 16:07:37 +0530

    Update server.py
    Signed-off-by: آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, send_from_directory, render_template, redirect, request
 import subprocess
+import io
+import sys
+import traceback
 
 app = Flask(__name__)
 
@@ -22,6 +25,39 @@ def run_code():
     else:
         return run.stderr
 
+@app.route('eval', methods=["POST"])
+def __eval():
+    text = request.data.decode("utf-8")
+    if not text:
+        return "Give code to Execute"
+    	OLDOUT = sys.stdout
+	OLDER = sys.stderr
+	NEWOUT = sys.stdout = io.StringIO()
+	NEWER = sys.stderr = io.StringIO()
+	stdout, stderr, exc, = None, None, None
+    try:
+		value = await aexec(args, event)
+	except Exception:
+        value = None
+		exc = traceback.format_exc()
+    NEWOUTT = NEWOUT.getvalue()
+	NEWERR = NEWER.getvalue()
+	sys.stdout = OLDOUT
+	sys.stderr = OLDER
+	edit = ''
+	if exc:
+		edit = exc
+	elif NEWOUTT:
+		edit = NEWOUTT
+	elif NEWERR:
+		edit = NEWERR
+	else:
+		edit = '<pre><code>SUCKSEXX</code></pre>'
+    #	final_output = "**EVAL : ♡**\n "
+	final_output = f"<pre><code>{args}</code></pre>"
+#	final_output += "**OUTPUT: ☆**\n"
+	final_output += f"<pre><code>{edit.strip()}</code></pre> \n"
+    return final_output
 
 @app.route('/<path:path>')
 def all_routes(path):
